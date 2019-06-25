ffed2cb3aef5cbb231e964631d74e1586c019f9a
Vikram Belde <symmetriccurve@gmail.com>
2019-06-25 10:19:52 -0500

    Create parse_objects.js

---
+++ Level 1/parse_objects.js
@@ -0,0 +1,27 @@
+Problem Statement: 
+
+Give a object below print the model,fuelType and seatsCount. 
+
+
+var vehicle = {
+  model: 2015,
+  engineInfo: {
+    fuelType: 'Petrol',
+    transmission: 'gear',
+    fuelCapacity: '20',
+    horsePower: '1000cc'
+  },
+  seatInfo: {
+    front: {
+      material: 'leather',
+      count: 2,
+      isHeated: true
+    },
+    back: {
+      material: 'leather',
+      count: 3,
+      isHeated: false
+    },
+  }
+  
+}
