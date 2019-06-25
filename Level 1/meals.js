6282120e3fc366b714f6fed355e1f73830bb99a0
Vikram Belde <symmetriccurve@gmail.com>
2019-06-25 13:30:50 -0500

    Update meals.js

--- Level 1/meals.js
+++ Level 1/meals.js
@@ -1,2 +1,29 @@
-Passed in time from 0 - 24, print the meals based on time passed into the function.
-if time is 0 to 8 (including), print  
+/* 
+
+## if else blocks
+
+Problem statement:
+Write a function that when passed in time from 0 - 24 as argument, print the type of meals based on time argument
+passed into the function.
+
+if time is 0 to 6 (including), print "Serving Early Breakfast"
+if time is 7 to 8 (including), print "Serving Breakfast"
+if time is 9 and 13 (including), print "Serving Lunch"
+if time is 14 to 17 (including), print "Serving Late Lunch"
+if time is 18 to 0 (excluding), print "Serving Dinner" 
+
+*/
+
+
+
+function serveMe(time){
+  // your code goes here
+}
+
+serveMe(0) // should print "Serving Early Breakfast"
+serveMe(6) // should print "Serving Early Breakfast"
+serveMe(7) // should print "Serving Breakfast"
+serveMe(9) // should print "Serving Lunch"
+serveMe(14) // should print "Serving Late Lunch"
+serveMe(18) // should print "Serving Dinner"
+
