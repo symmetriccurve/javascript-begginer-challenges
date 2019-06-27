2552e73056b57cd791aa2e3975f1e932e0a4337a
Vikram Belde <symmetriccurve@gmail.com>
2019-06-26 20:36:16 -0500

    Create calculator.js

---
+++ Level 1/calculator.js
@@ -0,0 +1,19 @@
+/* 
+  Write a function that takes two arguments and add those arguments and return the value of the sum
+/*
+
+function add(a,b){
+  // your code goes here
+}
+
+
+var firstAddition = add(2,3)
+console.log(firstAddition) // should print 5
+
+
+var secondAddition = add(10,-5)
+console.log(secondAddition) // should print 5
+
+
+var thirdAddition = add(0,10)
+console.log(firstAddition) // should print 10
