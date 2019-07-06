ebcad0d5b65b9f45d0bb9c0392b5a30b9f09a21f
Vikram Belde <symmetriccurve@gmail.com>
2019-07-05 23:13:28 -0500

    Update 2-object_mutation.js

--- Level 1/2-object_mutation.js
+++ Level 1/2-object_mutation.js
@@ -1,4 +1,12 @@
-/** Problem Statement
+/** Problem Statement 1
+ * Write a function to change the year 2013 to 2018  
+ * Write a function to change missionDuration from '6 months' to '12 months'
+ * Write a single function to change lowest,highest,inclination to 500 km , 76000 km and 200.0 degrees
+ * 
+ */
+
+
+/** Problem Statement 2
  * change/mutate the dryMass property 'unit' value from lb to kg 
  * convert 1064lbs into kgs and assign to the mass property of dryMass
  * 
