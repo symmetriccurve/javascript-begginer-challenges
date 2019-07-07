2a4b1140dc21609ea62db5f12ce3e63861c272da
Vikram <belvikram@gmail.com>
2019-07-07 00:21:46 -0500

    added array exercise

--- Level 1/5-function_return.js
+++ Level 1/5-function_return.js
@@ -1,51 +1,43 @@
 /** Problem Statement
- * Write a function to check if the launch year is 
- * 1. prints the year of launch of vehicle
- * 2. prints missionType
- * 3. prints highest orbit point.
+ * Write a function to check if the mission is Successful, if mission is Successful
+ * return true else false  and store it inside a variable 
+ * Print the variable value 
  */
- 
+
+
 var satellite = {
-  name: 'Mangalyaan',
-  year: 2013,
-  missionType: 'Mars orbiter',
-  isSuccessful: true,
-  missionDuration: '6 months',
-  launchSite: 'Satish Dhawan Space Centre',
-  orbitParameters: {
-      lowest: '421.7 km',
-      highest: '76,993.6 km',
-      inclination: '150.0 degrees'
-  },
-  instruments: [
-      'Atmospheric',
-      'Particle environment',
-      'Surface imaging'
-  ],
-  spaceCraftProperties: {
-      launchMass: {
-          mass: 1337.2,
-          unit: 'kg'
-      },
-      dryMass: {
-          mass: 1064,
-          unit: 'lb'
-      },
+    name: 'Mangalyaan',
+    year: 2013,
+    missionType: 'Mars orbiter',
+    isSuccessful: true,
+    missionDuration: '6 months',
+    launchSite: 'Satish Dhawan Space Centre',
+    orbitParameters: {
+        lowest: '421.7 km',
+        highest: '76,993.6 km',
+        inclination: '150.0 degrees'
+    },
+    instruments: [
+        'Atmospheric',
+        'Particle environment',
+        'Surface imaging'
+    ],
+    spaceCraftProperties: {
+        launchMass: {
+            mass: 1337.2,
+            unit: 'kg'
+        },
+        dryMass: {
+            mass: 1064,
+            unit: 'lb'
+        },
+    }
   }
+
+
+function isMissionSuccessful(){
+    // code here
 }
 
-function printYearOfLaunch(){
-    // your code goes here
-}
-
-function printMissionType(){
-    // your code goes here
-}
-
-function printHighestOrbitPoint(){
-    // your code goes here
-}
-
-printYearOfLaunch() // should print 2015
-printMissionType() // should print Mars orbiter
-printHighestOrbitPoint() // should print 76,993.6 km
+var result = isMissionSuccessful()
+console.log(result) // should print true
---
+++ Level 1/6-Arrays.js
@@ -0,0 +1,36 @@
+/** Exercise  1
+ * Print the length of the array
+ */
+
+ var satellites = ['Mangalyan','Aryabhata','YouthSat','Cartosat']
+
+
+ /** Exercise 2
+ * Write a function to print 'Yes, this is a indian satellite' if the given
+ *  satellite is from the list else print 'No' 
+ */
+
+var indianSatellites = ['Mangalyan','Aryabhata','YouthSat','Cartosat']
+
+ function isIndianSatellite(givenSatellite){
+     //Code here
+ }
+
+ console.log('Mangalyan') // should print 'Yes, this is a indian satellite'
+ console.log('Kosmos') // should print 'No, this is not a indian satellite'
+
+
+/** Exercise 3
+ * Write a function to print index of the satellite in the list 
+ */
+
+var indianSatellites = ['Mangalyan','Aryabhata','YouthSat','Cartosat']
+
+function getIndex(givenSatellite){
+    //Code here
+}
+
+console.log('Mangalyan') // should print 'The index of Mangalyan is 0' 
+console.log('Kosmos') // shoudl print 'The satellite do not exist in the list'
+
+ 
